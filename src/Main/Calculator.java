07db939868aeedb27f53c17649fa15861fe81442
orca <callstack@kakao.com>
2020-02-25 16:27:09 +0900

    [#3] 곱셈 구현 완료

--- src/Main/Calculator.java
+++ src/Main/Calculator.java
@@ -2,7 +2,8 @@ package Main;
 
 public class Calculator {
     public static int subtract(int num1, int num2) { return num1 - num2; }
-}
+
+    public static int multiply(int num1, int num2) { return num1 * num2; }
     static int add(int a, int b) {
         return a + b;
     }
